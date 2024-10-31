050db943f866d5ee1d010df39e30cf73896bd72b
aracool82 <aracool1982@gmail.com>
2024-10-31 16:58:50 +0700

    Initial commit

---
+++ Scripts/Counter.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using TMPro;
+using IEnumerator = System.Collections.IEnumerator;
+
+public class Counter : MonoBehaviour
+{
+    private WaitForSeconds _wait;
+    private Coroutine _coroutine;
+    private float _delay = .5f;
+    private int _counter = 0;
+
+    public event Action<int> OnCountUp;
+
+    private void Awake()
+    {
+        _wait = new WaitForSeconds(_delay);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_coroutine == null)
+            {
+                _coroutine = StartCoroutine(CounterUp());
+            }
+            else
+            {
+                StopCoroutine(_coroutine);
+                _coroutine = null;
+            }
+        }
+    }
+
+    private IEnumerator CounterUp()
+    {
+        while (true)
+        {
+            _counter++;
+            OnCountUp?.Invoke(_counter);
+            yield return _wait;
+        }
+    }
+}
---
+++ Scripts/CounterView.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class CounterView : MonoBehaviour
+{
+    private Counter _counter;
+    private TextMeshPro _text;
+
+    private void Awake()
+    {
+        _counter = GetComponent<Counter>();
+        _text = GetComponent<TextMeshPro>();
+    }
+
+    private void OnEnable()
+        => _counter.OnCountUp += OnCountUp;
+
+    private void OnDisable()
+        => _counter.OnCountUp -= OnCountUp;
+
+    private void OnCountUp(int counter)
+        => _text.text = counter.ToString();
+}
